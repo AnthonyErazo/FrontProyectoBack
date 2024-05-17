5d5a87a512bb20e64784eee76d06c4460133429b
Anthony Erazo <anthonyerazo76@gmail.com>
2024-05-17 00:16:26 -0500

    ruta al backend

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-// const REACT_APP_BASE_URL = "https://backend-proyecto-8ps3.onrender.com";
-const REACT_APP_BASE_URL = "http://localhost:8080";
+const REACT_APP_BASE_URL = "https://backend-proyecto-8ps3.onrender.com";
+// const REACT_APP_BASE_URL = "http://localhost:8080";
 
 export { REACT_APP_BASE_URL };
